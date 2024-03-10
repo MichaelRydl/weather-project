29d383d20a9fe8e6e662470b03a7e7b9b8a5df33
Michael Rýdl <Michael.rydl@gmail.com>
2024-03-10 10:13:47 +0100

    final theme changing

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,26 @@ import classes from "./App.module.css";
 import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
 import Side from "./components/Side/Side";
-import { Divider, MantineThemeProvider } from "@mantine/core";
+import cx from "clsx";
+import {
+  Divider,
+  MantineThemeProvider,
+  useComputedColorScheme,
+} from "@mantine/core";
 
 function App() {
+  const computedColorScheme = useComputedColorScheme("light", {
+    getInitialValueInEffect: true,
+  });
+
   return (
     <MantineThemeProvider>
-      <div className={classes.app}>
+      <div
+        className={cx(
+          classes.app,
+          computedColorScheme === "light" ? classes.dark : classes.light
+        )}
+      >
         <div className={classes.wrapper}>
           <div className={classes.header}>
             <Header />
