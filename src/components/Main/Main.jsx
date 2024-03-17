30731b4fec08a2bdb71a6e1ec42f11feab4d51a5
Michael Rýdl <Michael.rydl@gmail.com>
2024-03-17 15:00:48 +0100

    addin more informations and hourly sunshine duration

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useRecoilState, useRecoilValue } from "recoil";
 import { ActionIcon, Card, Flex, Loader, Text, Paper } from "@mantine/core";
+import { LineChart } from "@mantine/charts";
 import { IconStar } from "@tabler/icons-react";
 import { getTime } from "../../utils/utils";
 import { wmoCodes } from "../../../wmo-codes";
@@ -8,6 +9,22 @@ import axios from "axios";
 import classes from "./Main.module.css";
 import Sunrise from "../../assets/icons/line/sunrise.svg";
 import Sunset from "../../assets/icons/line/sunset.svg";
+import Wind from "../../assets/icons/wind.svg";
+import Humidity from "../../assets/icons/humidity.svg";
+import Umbrella from "../../assets/icons/umbrella.svg";
+import Barometer from "../../assets/icons/barometer.svg";
+import Cloudy from "../../assets/icons/cloudy.svg";
+import UVIndex1 from "../../assets/icons/uv-index-1.svg";
+import UVIndex2 from "../../assets/icons/uv-index-2.svg";
+import UVIndex3 from "../../assets/icons/uv-index-3.svg";
+import UVIndex4 from "../../assets/icons/uv-index-4.svg";
+import UVIndex5 from "../../assets/icons/uv-index-5.svg";
+import UVIndex6 from "../../assets/icons/uv-index-6.svg";
+import UVIndex7 from "../../assets/icons/uv-index-7.svg";
+import UVIndex8 from "../../assets/icons/uv-index-8.svg";
+import UVIndex9 from "../../assets/icons/uv-index-9.svg";
+import UVIndex10 from "../../assets/icons/uv-index-10.svg";
+import UVIndex11 from "../../assets/icons/uv-index-11.svg";
 import DayItem from "../DayItem/DayItem";
 import {
   favouriteLocationsState,
@@ -43,7 +60,7 @@ const Main = () => {
         const { latitude, longitude } = geolocationResponse.data.results[0];
 
         const weatherResponse = await axios.get(
-          `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,relative_humidity_2m,is_day,precipitation,rain,showers,snowfall,weather_code,cloud_cover,surface_pressure,wind_speed_10m&hourly=sunshine_duration&daily=weather_code,temperature_2m_max,temperature_2m_min,sunrise,sunset,precipitation_sum&timezone=auto&temperature_unit=${temperatureUnit}&wind_speed_unit=${windSpeedUnit}&precipitation_unit=${precipitationUnit}`
+          `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,relative_humidity_2m,is_day,precipitation,rain,showers,snowfall,weather_code,cloud_cover,surface_pressure,wind_speed_10m&hourly=sunshine_duration&daily=weather_code,temperature_2m_max,temperature_2m_min,sunrise,sunset,precipitation_sum,uv_index_max&timezone=auto&temperature_unit=${temperatureUnit}&wind_speed_unit=${windSpeedUnit}&precipitation_unit=${precipitationUnit}`
         );
 
         setWeatherData(weatherResponse.data);
@@ -76,6 +93,38 @@ const Main = () => {
       ...prevState,
       { name: geolocationData.name, country: geolocationData.country_code },
     ]);
+  };
+
+  const UVIndexIcons = [
+    UVIndex1,
+    UVIndex2,
+    UVIndex3,
+    UVIndex4,
+    UVIndex5,
+    UVIndex6,
+    UVIndex7,
+    UVIndex8,
+    UVIndex9,
+    UVIndex10,
+    UVIndex11,
+  ];
+
+  const getUVIndexIcon = (uvIndex) => {
+    const index = Math.min(Math.floor(uvIndex) - 1, UVIndexIcons.length - 1);
+    return UVIndexIcons[index];
+  };
+
+  console.log(weatherData);
+
+  const hourlySunshineDurationData = (sunshineDuration) => {
+    let hourlySunshineData = [];
+    for (let i = 0; i < 25; i++) {
+      hourlySunshineData.push({
+        time: `${i}h`,
+        Sunshine_Duration: Math.round((sunshineDuration[i] / 3600) * 100),
+      });
+    }
+    return hourlySunshineData;
   };
 
   return (
@@ -130,7 +179,7 @@ const Main = () => {
                     <Text size="4rem" c="white">
                       {Math.round(weatherData.current.temperature_2m)}
                       <sup style={{ fontSize: "2rem" }}>
-                        {temperatureUnit === "celsius" ? "°C" : "°F"}
+                        {weatherData.current_units.temperature_2m}
                       </sup>
                     </Text>
                   </Flex>
@@ -152,12 +201,87 @@ const Main = () => {
             )}
           </div>
         </Card>
-        <Paper
-          className={classes.weather_informations}
-          shadow="xl"
-          radius="xl"
-          p="lg"
-        ></Paper>
+        {weatherData ? (
+          <Paper shadow="xl" radius="xl" p="xl">
+            <Flex w="100%" h="100%" direction="column" justify="center">
+              <Flex
+                className={classes.weather_informations_item}
+                align="center"
+                gap="1rem"
+                wrap="wrap"
+              >
+                <img className={classes.weather_icon_info} src={Wind} alt="" />
+                <Text size="xl">{`${weatherData.current.wind_speed_10m}${weatherData.current_units.wind_speed_10m}`}</Text>
+              </Flex>
+              <Flex
+                className={classes.weather_informations_item}
+                align="center"
+                gap="1rem"
+                wrap="wrap"
+              >
+                <img
+                  className={classes.weather_icon_info}
+                  src={Humidity}
+                  alt=""
+                />
+                <Text size="xl">{`${weatherData.current.relative_humidity_2m}${weatherData.current_units.relative_humidity_2m}`}</Text>
+              </Flex>
+              <Flex
+                className={classes.weather_informations_item}
+                align="center"
+                gap="1rem"
+                wrap="wrap"
+              >
+                <img
+                  className={classes.weather_icon_info}
+                  src={Umbrella}
+                  alt=""
+                />
+                <Text size="xl">{`${weatherData.current.precipitation}${weatherData.current_units.precipitation}`}</Text>
+              </Flex>
+              <Flex
+                className={classes.weather_informations_item}
+                align="center"
+                gap="1rem"
+                wrap="wrap"
+              >
+                <img
+                  className={classes.weather_icon_info}
+                  src={Barometer}
+                  alt=""
+                />
+                <Text size="xl">{`${weatherData.current.surface_pressure}${weatherData.current_units.surface_pressure}`}</Text>
+              </Flex>
+              <Flex
+                className={classes.weather_informations_item}
+                align="center"
+                gap="1rem"
+                wrap="wrap"
+              >
+                <img
+                  className={classes.weather_icon_info}
+                  src={Cloudy}
+                  alt=""
+                />
+                <Text size="xl">{`${weatherData.current.cloud_cover}${weatherData.current_units.cloud_cover}`}</Text>
+              </Flex>
+              <Flex
+                className={classes.weather_informations_item}
+                align="center"
+                gap="1rem"
+                wrap="wrap"
+              >
+                <img
+                  className={classes.weather_icon_info}
+                  src={getUVIndexIcon(weatherData.daily.uv_index_max[0])}
+                  alt=""
+                />
+              </Flex>
+            </Flex>
+          </Paper>
+        ) : (
+          <Loader color="white" type="dots" size={50} />
+        )}
       </div>
       <div className={classes.wrapper_forecast}>
         <Flex justify="center" gap="1rem" wrap="wrap">
@@ -174,6 +298,23 @@ const Main = () => {
             <Loader color="white" type="dots" size={50} />
           )}
         </Flex>
+      </div>
+      <div>
+        {weatherData ? (
+          <LineChart
+            w="100%"
+            h={300}
+            data={hourlySunshineDurationData(
+              weatherData.hourly.sunshine_duration
+            )}
+            dataKey="time"
+            unit="%"
+            series={[{ name: "Sunshine_Duration", color: "yellow.6" }]}
+            curveType="monotone"
+          />
+        ) : (
+          <Loader w="100vw" color="gray" type="dots" size={50} />
+        )}
       </div>
     </div>
   );
